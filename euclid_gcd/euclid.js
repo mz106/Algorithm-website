848748df798cb0326b2b6eb2772bb7235879587f
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2021-06-03 20:21:10 +0100

    Add files via upload

---
+++ euclid_gcd/euclid.js
@@ -0,0 +1,69 @@
+
+const intOne = 452;
+const intTwo = 452;
+
+function oneBigger(par1, par2) {
+    console.log('function1')
+    let numerator = par1;
+    let denominator = par2;
+    const numeratorPrint = par1;
+    const denominatorPrint = par2;
+
+    let division = numerator % denominator;
+    
+
+    if(numerator % denominator === 0) {
+        console.log(`The GCD of ${numeratorPrint} and ${denominatorPrint} is ${denominatorPrint}`);
+    } else if(numerator % denominator !== 0) {
+        while (division > 0){
+            division = numerator % denominator;
+            numerator = denominator;
+            denominator = division;
+            
+            if(numerator % denominator === 0) {
+                console.log(`The GCD of ${numeratorPrint} and ${denominatorPrint} is ${denominator}`);
+            } 
+        }    
+    }
+        
+};
+
+function twoBigger(par1, par2) {
+    console.log("function two")
+    let numerator = par2;
+    let denominator = par1;
+    const numeratorPrint = par2;
+    const denominatorPrint = par1;
+
+    let division = numerator % denominator;
+    
+
+    if(numerator % denominator === 0) {
+        console.log(`The GCD of ${numeratorPrint} and ${denominatorPrint} is ${denominatorPrint}`);
+    } else if(numerator % denominator !== 0) {
+        while (division > 0){
+            division = numerator % denominator;
+            numerator = denominator;
+            denominator = division;
+            
+            if(numerator % denominator === 0) {
+                console.log(`The GCD of ${denominatorPrint} and ${numeratorPrint} is ${denominator}`);
+            } 
+        }    
+    }
+};
+
+function euclid(par1, par2) {
+    let parameter1 = par1;
+    let parameter2 = par2;
+
+    if(par1 > par2) {
+        oneBigger(parameter1, parameter2);
+    } else if(par2 > par1) {
+        twoBigger(par2, par1);
+    } else if(par1 === par2) {
+        console.log("Please enter two different numbers");
+    }
+};
+
+euclid(intOne, intTwo)
